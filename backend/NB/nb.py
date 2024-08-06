5e6b28c12962227f7fe6a307d46c1164e41b95c1
newmanhw <newmanhwaters@gmail.com>
2024-08-06 01:39:10 -0400

    finalized nb.py

---
+++ backend/NB/nb.py
@@ -0,0 +1,32 @@
+import joblib
+import os
+import re
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+nltk.download("stopwords")
+
+# Load NB Model:
+nb_path = os.path.join(os.path.dirname(__file__), "../naive-bayes/nb_model.pkl")
+nb_model = joblib.load(nb_path)
+
+# Message processor:
+def process_message(message):
+    message = message.lower()
+    message = word_tokenize(message)
+    message = [word.strip() for word in message]
+    message = [word.replace("\n", "") for word in message]
+    message = [re.sub(r"\d+", "", word) for word in message]
+    message = [re.sub(r"[^a-zA-Z0-9\s]", "", word) for word in message]
+    stop_words = set(stopwords.words("english"))
+    message = [word for word in message if word not in stop_words]
+    ps = PorterStemmer()
+    message = [ps.stem(word) for word in message]
+    return " ".join(message)
+
+# Process email and run through model
+def run_nb(message):
+    processed_message = process_message(message)
+    classified = nb_model.predict(message)
+    return classified
